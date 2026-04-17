50f2d4c338605ae55bfb56f5572f67e71480cbde
Omika Kumar <kumaromika007@gmail.com>
2026-04-17 14:21:41 +0530

    experiment5

--- ok/src/App.jsx
+++ ok/src/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import Studentinfo from "./Experiment4/Studentinfo";
+// import Studentinfo from "./Experiment4/Studentinfo";
+import Counter from "./Experiment5/Counter";
+// function App() {
+//   return (
+//     <div>
+//       <h1>Student Information</h1>
 
+//       <Studentinfo name="Omika" course="B.Tech" marks="95" />
+//       <Studentinfo name="Rahul" course="B.Com" marks="90" />
+//       <Studentinfo name="Ananya" course="BCA" marks="88" />
+
+//     </div>
+//   );
+// }
 function App() {
-  return (
-    <div>
-      <h1>Student Information</h1>
-
-      <Studentinfo name="Omika" course="B.Tech" marks="95" />
-      <Studentinfo name="Rahul" course="B.Com" marks="90" />
-      <Studentinfo name="Ananya" course="BCA" marks="88" />
-
-    </div>
+  return(
+     <>
+      <Counter />
+    </>
   );
 }
 
