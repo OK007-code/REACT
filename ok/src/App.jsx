a694b3faf5206733424b740e04f4fde7b0ec271e
Omika Kumar <kumaromika007@gmail.com>
2026-05-08 09:26:28 +0530

    bootstrap

--- ok/src/App.jsx
+++ ok/src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import Studentinfo from "./Experiment4/Studentinfo";
+// import Studentinfo from "./Experiment4/Studentinfo";
 // import Counter from "./Experiment5/Counter";
-// import Form from "./Experiment6/Form";
-function App() {
-  return (
-    <div>
-      <h1>Student Information</h1>
+import Form from "./Experiment6/Form";
+// function App() {
+//   return (
+//     <div>
+//       <h1>Student Information</h1>
 
-      <Studentinfo name="Omika" course="B.Tech" marks="95" />
+//       <Studentinfo name="Omika" course="B.Tech" marks="95" />
 
-      <Studentinfo name="Rahul" course="B.Com" marks="90" />
-      <Studentinfo name="Ananya" course="BCA" marks="88" />
+//       <Studentinfo name="Rahul" course="B.Com" marks="90" />
+//       <Studentinfo name="Ananya" course="BCA" marks="88" />
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 // function App() {
 //   return(
 //      <>
@@ -23,13 +23,13 @@ function App() {
 //   );
 // }
 
-export default App;
-// function App() {
-//   return (
-//     <>
-//       <Form />
-//     </>
-//   );
-// }
-
 // export default App;
+function App() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+export default App;
